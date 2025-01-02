db2bc86c146127ac26ed37a7a26581ba9da2d173
Chad <laban.chad@gmail.com>
2025-01-02 13:54:46 +0800

    Add: Seed file for project.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Enum\RolesEnum;
+use App\Enum\PermissionsEnum;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +16,54 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // seed for user roles & permissions
+        $guestRole = Role::create(['name' => RolesEnum::Guest->value]);
+        $adminRole = Role::create(['name' => RolesEnum::Admin->value]);
+        $userRole = Role::create(['name' => RolesEnum::User->value]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $manageRecipesPermission = Permission::create([
+            'name' => PermissionsEnum::ManageRecipes->value,
         ]);
+
+        $manageUsersPermission = Permission::create([
+            'name' => PermissionsEnum::ManageUsers->value,
+        ]);
+
+        $userRole->syncPermissions([$manageRecipesPermission]);
+        $adminRole->syncPermissions([
+            $manageRecipesPermission,
+            $manageUsersPermission,
+        ]);
+
+        // create users
+        $guest = User::factory()->create([
+            'name' => 'Guest User',
+            'email' => 'guest@example.com',
+            'password' => bcrypt('performativ1234'),
+        ])->assignRole(RolesEnum::Guest);
+
+        $user1 = User::factory()->create([
+            'name' => 'Chad Doe',
+            'email' => 'user1@example.com',
+            'password' => bcrypt('performativ1234u1'),
+        ])->assignRole(RolesEnum::User);
+
+        $user2 = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'user2@example.com',
+            'password' => bcrypt('performativ1234u2'),
+        ])->assignRole(RolesEnum::User);
+
+        $user3 = User::factory()->create([
+            'name' => 'Jane Doe',
+            'email' => 'user3@example.com',
+            'password' => bcrypt('performativ1234u3'),
+        ])->assignRole(RolesEnum::User);
+
+        $admin = User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('performativAdmin1234'),
+        ])->assignRole(RolesEnum::Admin);
     }
 }
