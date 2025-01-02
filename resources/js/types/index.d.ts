e5a17969af149ee9b00a159239529616f7673cad
Chad <laban.chad@gmail.com>
2025-01-02 17:47:53 +0800

    Add: Type definitions.

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -4,7 +4,46 @@ export interface User {
     id: number;
     name: string;
     email: string;
-    email_verified_at?: string;
+    permissions: string[];
+    roles: string[];
+}
+
+export interface InitUser {
+    auth: {
+        user: User;
+    };
+}
+
+export interface Recipe {
+    id: number;
+    name: string;
+    description: string;
+    category_id: number | string;
+    ingredients: string[];
+    instructions: string[];
+    created_by: string;
+    created_at: Date;
+}
+
+export interface Category {
+    id: number;
+    name: string;
+}
+
+export interface RecipeModalProps {
+    recipe: Recipe | null;
+    closeModal: () => void;
+}
+
+export interface NutritionalValues {
+    fat_total_g: number;
+    fat_saturated_g: number;
+    sodium_mg: number;
+    potassium_mg: number;
+    cholesterol_mg: number;
+    carbohydrates_total_g: number;
+    fiber_g: number;
+    sugar_g: number;
 }
 
 export type PageProps<
